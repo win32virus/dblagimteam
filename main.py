a25a93ee55f9f773bf77962ff1e85b1da2013996
mungsul <hukju0714@gmail.com>
2018-04-26 19:48:57 +0900

    make test file

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/<name>')
+@app.route('/hello<name>')
 def hello(name):
     return render_template('hello.html', name=name)
 
@@ -11,6 +11,10 @@ def hello(name):
 @app.route('/')
 def index():
     return "hello, world"
+
+@app.route('/test',methods=['GET'])
+def test():
+    return request.args.get('user')
 
 if __name__ == '__main__':
     app.run('0.0.0.0',5000)
