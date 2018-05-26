e93d3fa3c1c61e204686669aef3c0323a06c18e0
mungsul <hukju0714@gmail.com>
2018-05-26 22:00:10 +0900

    make login, join api, search by coordinate

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#-*-coding:utf-8
 from flask import Flask, request, render_template
 import pymysql
 import json
@@ -17,37 +18,82 @@ def join():
     return render_template('join.html')
 
 
-@app.route('/user', methods=['GET', 'POST'])
-def user():
-    if request.method == 'GET':
-        user_id = request.args.get('id')
-        user_pw = request.args.get('pw')
-        query = "select * from user where user_id = '{0}' and user_pw = '{1}'".format(user_id, user_pw)
+@app.route('/map')
+def map_test():
+    return render_template('map.html')
 
-        if user_id is None or user_pw is None:
-            return "id or pw error"
 
-    elif request.method == 'POST':
-        user_id = request.form['id']
-        user_pw = request.form['pw']
-        user_name = request.form['name']
-        user_email = request.form['email']
-        user_phone = request.form['phone']
+@app.route('/login', methods=['POST'])
+def login():
+    user_id = request.form['id']
+    user_pw = request.form['pw']
+    query = "select user_id from user where user_id = %s and user_pw = %s"
 
-        if user_id is None or user_pw is None or user_name is None or user_email is None or user_phone is None:
-            return "Error"
-
-        dup_check_query = "select user_id from user where user_id = '{0}'".format(user_id)
-        dup_result = cur.execute(dup_check_query)
-        if dup_result >= 1:
-            return "already exist"
-
-        query = "insert into user(user_id, user_pw, user_name, user_email, user_phone) values('{0}', '{1}', '{2}'," \
-                "'{3}', '{4}')".format(user_id, user_pw, user_name, user_email, user_phone)
-    query_result = cur.execute(query)
-    result = {'result': query_result, 'query': query}
+    if user_id is None or user_pw is None:
+        result = {'result': 'id or pw error'}
+    else:
+        query_result = cur.execute(query, (user_id, user_pw))
+        result = {'result': query_result}
     return json.dumps(result)
 
 
+@app.route('/user', methods=['POST'])
+def create_user():
+    user_id = request.form['id']
+    user_pw = request.form['pw']
+    user_name = request.form['name']
+    user_email = request.form['phone']
+
+    if user_id is None or user_pw is None or user_name is None or user_email is None:
+        result = {'result': 'value error'}
+    else:
+        query = "insert into user(user_id, user_pw, user_name, user_email, user_phone) " \
+                "values(%s, password(%s), %s, %s, %s)"
+        query_result = cur.execute(query, (user_id, user_pw, user_name, user_email))
+        result = {'result': query_result}
+    return json.dumps(result)
+
+
+@app.route('/post/<int:pid>', methods=['GET'])
+def get_post(pid):
+    post_idx = pid
+    query = "select idx, post_title, post_contents, post_writer, post_requester, post_goods_type, post_rental_status, "\
+            "post_rental_duration, post_rental_price from post where idx=%s"
+    cur.execute(query, (int(post_idx)))
+    query_result = cur.fetchone()
+    result = {'result': query_result}
+    return json.dumps(result)
+
+
+@app.route('/post', methods=['POST'])
+def create_post():
+    post_title = request.form['title']  # string
+    post_contents = request.form['contents']  # string
+    post_writer = request.form['writer']  # int
+    post_goods_type = request.form['goods_type']  # int
+    post_rental_duration = request.form['duration']  # per day, int
+    post_rental_price = request.form['price']  # won, int
+    query = "insert into post(post_title, post_contents, post_writer, post_goods_type, post_rental_duration," \
+            "post_rental_duration, post_rental_price) values(%s, %s, %s, %s, %s, %s, %s)"
+    query_result = cur.execute(query, (post_title, post_contents, int(post_writer), int(post_goods_type),
+                                       int(post_rental_duration), int(post_rental_price)))
+    result = {'result': query_result}
+    return json.dumps(result)
+
+
+@app.route('/search/<string:coordinate>', methods=['GET'])
+def search_post(coordinate):
+    try:
+        query = "select idx from post where ST_Distance(post_geom, POINT({0})) < 10".format(coordinate)
+        cur.execute(query)
+    except:
+        result = {'result': 'query error'}
+        return json.dumps(result)
+    else:
+        query_result = cur.fetchone()
+        result = {'result': query_result}
+        return json.dumps(result)
+
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000, debug=True)
+    app.run('0.0.0.0', 8888)
