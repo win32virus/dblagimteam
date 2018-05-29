474777117215c929ec43157fedb471d95f9a6bf1
God Sang <tkdgh583@naver.com>
2018-05-29 23:06:53 +0900

    글쓰기 -> post 글 조회 -> show

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
 
+@app.route('/user')
+def showUserName():
+    return render_template('user.html', name=session['userName'])
 
-@app.route('/hello<name>')
-def hello(name):
-    return render_template('hello.html', name=name)
 
+@app.route('/post')
+def postScreen():
+    return render_template('post.html')
+
+@app.route('/show')
+def showScreen():
+    return render_template('show.html')
 
 @app.route('/')
-def index():
-    return "hello, world"
+def login_form() :
+    return render_template('login_form.html')
 
-@app.route('/test',methods=['GET'])
-def test():
-    return request.args.get('user')
+@app.route('/login', methods=['POST'])
+def login() :
+    if request.method== 'POST' :
+        session['userName']=request.form['userName']
+        return redirect(url_for('showUserName'))
+    else :
+        return 'login failed'
 
-if __name__ == '__main__':
-    app.run('0.0.0.0',5000)
+app.secret_key='abcdefsdfasdffdf'
+
+if __name__ == "__main__":
+    app.run('0.0.0.0',7700)
